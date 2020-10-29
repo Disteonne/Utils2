0b5677a0edfabdf3a67a76002bfbcc32e7861575
Disteonne <diana.razmikovna@yandex.ru>
2020-10-29 10:59:39 +0300

    utils new vers

--- src/main/java/urlDown/MainMethod.java
+++ src/main/java/urlDown/MainMethod.java
@@ -4,8 +4,8 @@ package urlDown;
  */
 public class MainMethod {
     public static void main(String[] args) {
-    String[] argTest={"https://sun9-17.userapi.com/EQB7ucmqFPcyt8jrqB7lNxscqO1-Lc8GKMQltw/60EBCUxu0z8.jpg","C:\\Users\\huawei\\Desktop\\TestURL\\Photo","open"};
+    //String[] argTest={"https://sun9-17.userapi.com/EQB7ucmqFPcyt8jrqB7lNxscqO1-Lc8GKMQltw/60EBCUxu0z8.jpg","C:\\Users\\huawei\\Desktop\\TestURL\\Photo","open"};
     Start start=new Start();
-    start.start(argTest);
+    start.start(args);
     }
 }
